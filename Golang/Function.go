859e17889ce4cb9d16f96e0a348bbe12fbe1cc1e
Mr. Python <myselfvishal7@gmail.com>
2021-07-03 16:18:38 +0530

    Add files via upload

---
+++ Golang/Function.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func add(x int, y int) {
+	total := 0
+	total = x + y
+	fmt.Println(total)
+}
+
+func main() {
+
+	add(20, 30)
+}
---
+++ Golang/Loops2.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 100
+
+	if x == 50 {
+		fmt.Println("India")
+	} else if x == 100 {
+		fmt.Println("China")
+	} else {
+		fmt.Println("Srilanka")
+	}
+}
